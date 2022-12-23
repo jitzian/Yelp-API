ffbba66ab5186d7231134ef68a6bebf56dc4d858
joNA <jitzian@gmail.com>
2022-12-22 22:17:40 -0600

    yelp jc - Code cleanup

--- app/src/main/java/com/example/yelpcode/utils/Utils.kt
+++ app/src/main/java/com/example/yelpcode/utils/Utils.kt
@@ -1,13 +1,6 @@
 package com.example.yelpcode.utils
 
 /**
- * Function for simplifying couple of null checks and avoid nested a?.let { b?.let{ .. }}
- * */
-fun <T1 : Any, T2 : Any, R : Any> safeLet(p1: T1?, p2: T2?, block: (T1, T2) -> R?): R? {
-    return if (p1 != null && p2 != null) block(p1, p2) else null
-}
-
-/**
  * Small function to retrieve TAG and avoid unnecessary declarations on each class
  * */
 inline fun <reified T> T.TAG(): String = T::class.java.simpleName
