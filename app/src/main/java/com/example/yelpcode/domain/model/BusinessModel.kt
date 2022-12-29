59a425e8f1e09e9ed87b4d52022947be9653260e
joNA <jitzian@gmail.com>
2022-12-29 14:19:27 -0600

    yelp jc - Incorporating Error iinto BusinessModel mapping object

--- app/src/main/java/com/example/yelpcode/domain/model/BusinessModel.kt
+++ app/src/main/java/com/example/yelpcode/domain/model/BusinessModel.kt
@@ -1,4 +1,5 @@
 package com.example.yelpcode.domain.model
+import com.example.yelpcode.data.remote.model.Error
 
 data class BusinessModel(
     val id: String? = "",
@@ -9,5 +10,6 @@ data class BusinessModel(
     val price: String = "$-",
     val rating: Double? = 0.0,
     val phone: String? = "N/A",
-    val address: String? = "N/A"
+    val address: String? = "N/A",
+    val error: Error? = null
 )
