8f9817d38451e002d85b0633e0869bd5f6f4ba4e
joNA <jitzian@gmail.com>
2022-12-29 15:23:36 -0600

    yelp jc - Refactoring of Details screen as if it being done in beers and pizza place

--- app/src/main/java/com/example/yelpcode/ui/screens/details/viewmodels/DetailsViewModel.kt
+++ app/src/main/java/com/example/yelpcode/ui/screens/details/viewmodels/DetailsViewModel.kt
@@ -1,60 +1,36 @@
 package com.example.yelpcode.ui.screens.details.viewmodels
 
-import android.app.Application
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.yelpcode.R
-import com.example.yelpcode.constants.GlobalConstants
-import com.example.yelpcode.data.remote.model.Businesse
+import com.example.yelpcode.domain.model.BusinessModel
 import com.example.yelpcode.domain.repository.BusinessRepository
-import com.example.yelpcode.utils.TAG
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withTimeout
 import javax.inject.Inject
+
 
 @HiltViewModel
 class DetailsViewModel @Inject constructor(
-    private val application: Application,
     private val repository: BusinessRepository
 ) : ViewModel() {
 
-    private val _state: MutableStateFlow<UIState> = MutableStateFlow(UIState.Loading)
-    val state: StateFlow<UIState>
-        get() = _state.asStateFlow()
+    private val _state = MutableStateFlow(UIState(loading = true))
+    val state = _state.asStateFlow()
 
+    /**
+     * [id]: Identifier for specific Business / Place
+     * */
     fun fetchDetailsById(id: String) = viewModelScope.launch(Dispatchers.IO) {
-        try {
-            withTimeout(GlobalConstants.MAX_TIME_OUT) {
-                if (state.value == UIState.Loading) {
-                    val data = repository.fetchBusinessById(id = id)
-                    _state.value = UIState.Success(
-                        data = data
-                    )
-                }
-            }
-        } catch (tce: TimeoutCancellationException) {
-            Log.e(this@DetailsViewModel.TAG(), "fetchDetailsById::tce::${tce.message}")
-            _state.value = UIState.Error(
-                message = tce.message ?: application.getString(R.string.time_out_error_TEXT)
-            )
-        } catch (e: Exception) {
-            Log.e(this@DetailsViewModel.TAG(), "fetchDetailsById:::e::${e.message}")
-            _state.value = UIState.Error(
-                message = e.message ?: application.getString(R.string.generic_error_TEXT)
-            )
-        }
+        val place = repository.fetchBusinessById(id)
+        _state.value = UIState(true)
+        _state.value = UIState(data = place)
     }
 
-    sealed class UIState {
-        object Loading : UIState()
-        class Success(val data: Businesse) : UIState()
-        class Error(val message: String) : UIState()
-    }
+    data class UIState(
+        val loading: Boolean = false,
+        val data: BusinessModel = BusinessModel()
+    )
 }
--- app/src/main/java/com/example/yelpcode/ui/screens/details/views/DetailsScreen.kt
+++ app/src/main/java/com/example/yelpcode/ui/screens/details/views/DetailsScreen.kt
@@ -16,15 +16,14 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberImagePainter
 import com.example.yelpcode.R
-import com.example.yelpcode.data.remote.model.Businesse
+import com.example.yelpcode.domain.model.BusinessModel
 import com.example.yelpcode.ui.app.YelpAppScreen
-import com.example.yelpcode.ui.common.ErrorScreen
 import com.example.yelpcode.ui.common.LoadingScreen
 import com.example.yelpcode.ui.common.MainTopBar
+import com.example.yelpcode.ui.screens.businesses.businessError.BusinessError
 import com.example.yelpcode.ui.screens.details.viewmodels.DetailsViewModel
 
 @Composable
@@ -37,24 +36,19 @@ fun DetailsScreenState(
     val state by detailsViewModel.state.collectAsState()
     detailsViewModel.fetchDetailsById(id)
 
-    when (state) {
-        is DetailsViewModel.UIState.Loading -> {
-            LoadingScreen()
-        }
-        is DetailsViewModel.UIState.Error -> {
-            ErrorScreen(message = (state as DetailsViewModel.UIState.Error).message)
-        }
-        is DetailsViewModel.UIState.Success -> {
-            DetailsScreen(
-                onBackClick = onBackClick,
-                data = (state as DetailsViewModel.UIState.Success).data
-            )
-        }
-    }
+    DetailsScreen(
+        onBackClick = onBackClick,
+        loading = state.loading,
+        data = state.data
+    )
 }
 
 @Composable
-fun DetailsScreen(onBackClick: () -> Unit, data: Businesse) {
+fun DetailsScreen(
+    onBackClick: () -> Unit,
+    loading: Boolean,
+    data: BusinessModel
+) {
     YelpAppScreen {
         Scaffold(topBar = {
             MainTopBar(
@@ -63,42 +57,39 @@ fun DetailsScreen(onBackClick: () -> Unit, data: Businesse) {
                 onBackClick = onBackClick
             )
         }) {
-            val scrollState = rememberScrollState()
-            Box(modifier = Modifier.fillMaxWidth()) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .verticalScroll(state = scrollState)
-                        .padding(all = dimensionResource(id = R.dimen.dimen_16_dp)),
-                    verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.dimen_8_dp))
-                ) {
-                    Image(
-                        painter = rememberImagePainter(data = data.imageUrl),
-                        contentDescription = data.name,
+            if (loading) {
+                LoadingScreen()
+            }
+
+            if (data.error != null) {
+                BusinessError(errorType = data.error)
+            } else {
+                val scrollState = rememberScrollState()
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    Column(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .aspectRatio(1f)
-                            .clip(RoundedCornerShape(dimensionResource(id = R.dimen.dimen_4_dp))),
-                        contentScale = ContentScale.FillBounds
-                    )
-                    Text(
-                        text = data.name ?: stringResource(id = R.string.n_a_TEXT),
-                        modifier = Modifier.fillMaxWidth(),
-                        style = MaterialTheme.typography.h6
-                    )
-                    Text(
-                        text = data.alias ?: stringResource(id = R.string.n_a_TEXT),
-                        modifier = Modifier.fillMaxWidth(),
-                        style = MaterialTheme.typography.caption
-                    )
+                            .verticalScroll(state = scrollState)
+                            .padding(all = dimensionResource(id = R.dimen.dimen_16_dp)),
+                        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.dimen_8_dp))
+                    ) {
+                        Image(
+                            painter = rememberImagePainter(data = data.imageUrl),
+                            contentDescription = data.name,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(1f)
+                                .clip(RoundedCornerShape(dimensionResource(id = R.dimen.dimen_4_dp))),
+                            contentScale = ContentScale.FillBounds
+                        )
+                        Text(
+                            text = data.name ?: stringResource(id = R.string.n_a_TEXT),
+                            modifier = Modifier.fillMaxWidth(),
+                            style = MaterialTheme.typography.h6
+                        )
+                    }
                 }
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PrevDetailsScreen() {
-
 }
